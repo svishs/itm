7d0c9cf2f455aa5e21b07aae3103ccb1878f7df7
svishs <vishnichenko.sergey@gmail.com>
2024-04-27 10:17:37 +0300

    dj 12 template loop if

--- t10_django/balakirev/sitewomen/sitewomen/settings.py
+++ t10_django/balakirev/sitewomen/sitewomen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5-w@sy66seiu#qxas+-ooba4nbhvwlr%o=$u)(p)3b70&=mi8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
--- t10_django/balakirev/sitewomen/women/views.py
+++ t10_django/balakirev/sitewomen/women/views.py
@@ -5,12 +5,16 @@ from django.template.loader import render_to_string
 
 # Create your views here.
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 def index(request): # HttpRequest
     # t = render_to_string('women/index.html')
@@ -18,11 +22,7 @@ def index(request): # HttpRequest
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'float': 28.56,
-        'lst': [1, 2, 'abc', True],
-        'set': {1, 2, 3, 2, 5},
-        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10, 20),
+        'posts': data_db,
     }
     return render(request,'women/index.html', context=data)
     # return HttpResponse("Страница приложения women") # прописываем маршрут для
