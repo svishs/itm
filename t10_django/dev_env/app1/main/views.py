ee35ab1b1623aefee34f2d763f5cbebcc3a88ae3
svishs <vishnichenko.sergey@gmail.com>
2024-05-07 10:17:03 +0300

    load goods, creating fixtures

--- t10_django/dev_env/app1/main/views.py
+++ t10_django/dev_env/app1/main/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+# подтягиваем модели 
+from goods.models import Categories
 
 # Create your views here.
 
 def index(request):
+
+    categories = Categories.objects.all()
     context= {
         'title': 'Home - Главная',
         'content': 'Магазин мебели - HOME',
+        'categories': categories,
 
     }
     return render(request, 'main/index.html', context=context)
