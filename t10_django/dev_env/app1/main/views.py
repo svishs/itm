711e72512dbb6d203f63deaeea279154ff1c6a29
svishs <vishnichenko.sergey@gmail.com>
2024-05-08 11:09:30 +0300

    registered simple tag for categories

--- t10_django/dev_env/app1/main/views.py
+++ t10_django/dev_env/app1/main/views.py
@@ -7,11 +7,11 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
+    # categories = Categories.objects.all()
     context= {
         'title': 'Home - Главная',
         'content': 'Магазин мебели - HOME',
-        'categories': categories,
+        # 'categories': categories,
 
     }
     return render(request, 'main/index.html', context=context)
