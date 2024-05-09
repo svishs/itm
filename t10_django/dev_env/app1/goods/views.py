1a3ae746db82f1afe0c5e432a2fbf64e3d9f7f65
svishs <vishnichenko.sergey@gmail.com>
2024-05-09 15:19:44 +0300

    first pagination option

--- t10_django/dev_env/app1/goods/views.py
+++ t10_django/dev_env/app1/goods/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from goods.models import Products
 
 
-def catalog(request):
-    
-    goods = Products.objects.all()
+def catalog(request, category_slug, page=1):   
+    if category_slug=='all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, per_page= 3)
+    current_page = paginator.page(page)
+
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
