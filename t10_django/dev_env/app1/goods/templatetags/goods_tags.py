3d00b4b01f670b9f71b509e71125c23a10e89983
svishs <vishnichenko.sergey@gmail.com>
2024-05-10 19:54:45 +0300

    postgreSQL fulltext search

--- t10_django/dev_env/app1/goods/templatetags/goods_tags.py
+++ t10_django/dev_env/app1/goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 from goods.models import Categories
 
 register = template.Library()
@@ -6,4 +7,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
 
---
+++ t10_django/dev_env/app1/goods/utils.py
@@ -0,0 +1,20 @@
+
+from django.db.models import Q  
+from goods.models import Products
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+
+def q_search(query):
+    if query.isdigit() and len(query)<=5:
+        return Products.objects.filter(id=int(query))
+
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    # keywords = [word for word in query.split() if len(word)>2 ]
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
