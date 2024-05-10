2c7c441e815a3df875a1982b792ccf7e788eb623
svishs <vishnichenko.sergey@gmail.com>
2024-05-10 20:37:48 +0300

    full text search done

--- t10_django/dev_env/app1/goods/utils.py
+++ t10_django/dev_env/app1/goods/utils.py
@@ -1,18 +1,39 @@
 
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 from django.db.models import Q  
 from goods.models import Products
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 def q_search(query):
-    if query.isdigit() and len(query)<=5:
+
+    if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    # keywords = [word for word in query.split() if len(word)>2 ]
-    # q_objects = Q()
 
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
     # for token in keywords:
     #     q_objects |= Q(description__icontains=token)
     #     q_objects |= Q(name__icontains=token)
